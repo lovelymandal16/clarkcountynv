23eb6839d884406a3ec1555beee39401bbce7cbf
Meet Kumar Jain <meejain@p01kbdb03.macromedia.com>
2024-11-30 05:21:14 +0530

    Initializing scripts for calendar

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -21,6 +21,36 @@ function googleTranslate() {
 
 googleTranslate();
 
+// async function fc1() {
+//   const s1 = document.createElement('script');
+//   s1.setAttribute('src', 'https://cdn.jsdelivr.net/npm/fullcalendar@5.1.0/main.min.js');
+//   (document.body || document.head).appendChild(s1);
+// }
+
+// await fc1();
+
+// async function fc1() {
+//   await loadScript('https://cdn.jsdelivr.net/npm/fullcalendar/index.global.min.js');
+// }
+
+// await fc1();
+
+// async function fc2() {
+//   const s2 = document.createElement('script');
+//   s2.text = `
+//     document.addEventListener('DOMContentLoaded', function() {
+//         const calendarEl = document.getElementById('calendar')
+//         const calendar = new FullCalendar.Calendar(calendarEl, {
+//           initialView: 'dayGridMonth'
+//         })
+//         calendar.render()
+//       })
+//   `;
+//   (document.body || document.head).appendChild(s2);
+// }
+
+// await fc2();
+
 async function loadWidget() {
   await loadScript('/widgets/accessibility/accessibility.js');
 }

f8159ae8283d97fd128a9efbe5542e16d8ae57c0
Meet Kumar Jain <meejain@p01kbdb03.macromedia.com>
2024-12-01 00:35:39 +0530

    Instrumenting a Template - Calendar

--- templates/calendar/calendar.js
+++ templates/calendar/calendar.js
@@ -10,6 +10,59 @@ export function loadfullcalendar() {
     // eslint-disable-next-line no-undef
     const calendar = new FullCalendar.Calendar(calendarEl, {
       initialView: 'dayGridMonth',
+      headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay',
+      },
+      events: [
+        {
+          title: 'All Day Event',
+          start: '2024-11-01',
+        },
+        {
+          title: 'Long Event',
+          start: '2024-11-07',
+          end: '2024-11-10',
+        },
+        {
+          groupId: '999',
+          title: 'Repeating Event',
+          start: '2024-11-09T16:00:00',
+        },
+        {
+          groupId: '999',
+          title: 'Repeating Event',
+          start: '2024-11-16T16:00:00',
+        },
+        {
+          title: 'Conference',
+          start: '2024-11-11',
+          end: '2024-11-13',
+        },
+        {
+          title: 'Meeting',
+          start: '2024-11-12T10:30:00',
+          end: '2024-11-12T12:30:00',
+        },
+        {
+          title: 'Lunch',
+          start: '2024-11-12T12:00:00',
+        },
+        {
+          title: 'Meeting',
+          start: '2024-11-12T14:30:00',
+        },
+        {
+          title: 'Birthday Party',
+          start: '2024-11-13T07:00:00',
+        },
+        {
+          title: 'Click for Google',
+          url: 'https://google.com/',
+          start: '2024-11-28',
+        },
+      ],
     });
     calendar.render();
   });

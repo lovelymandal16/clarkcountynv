5451d8473aec6d1e659eae6452e94f4ca1b0efb0
Meet Kumar Jain <meejain@p01kbdb03.macromedia.com>
2024-12-01 03:21:12 +0530

    Instrumenting a Template - Calendar

--- templates/calendar/calendar.js
+++ templates/calendar/calendar.js
@@ -2,8 +2,73 @@ import { div, section } from '../../scripts/dom-helpers.js';
 
 // Scripts for Full Calendar
 
-function draw() {
+function getEvents() {
+  const events = [
+    {
+      title: 'All Day Event',
+      start: '2024-12-01',
+      backgroundColor: '#3787d8',
+    },
+    {
+      title: 'Long Event',
+      start: '2024-12-07',
+      end: '2024-12-10',
+      backgroundColor: '#da80c1',
+    },
+    {
+      groupId: '999',
+      title: 'Repeating Event',
+      start: '2024-12-09T16:00:00',
+      backgroundColor: '#3a9500',
+    },
+    {
+      groupId: '1000',
+      title: 'Repeating Event',
+      start: '2024-12-16T16:00:00',
+      end: '2024-12-16T18:00:00',
+      backgroundColor: '#3a9500',
+      borderColor: 'blue',
+    },
+    {
+      title: 'Conference',
+      start: '2024-12-11',
+      end: '2024-12-13',
+      backgroundColor: 'red',
+    },
+    {
+      title: 'Meeting',
+      start: '2024-12-12T10:30:00',
+      end: '2024-12-12T12:30:00',
+      backgroundColor: 'red',
+    },
+    {
+      title: 'Lunch',
+      start: '2024-12-12T12:00:00',
+      backgroundColor: 'red',
+    },
+    {
+      title: 'Meeting',
+      start: '2024-12-12T14:30:00',
+      backgroundColor: 'red',
+    },
+    {
+      title: 'Birthday Party',
+      start: '2024-12-13T07:00:00',
+      backgroundColor: 'red',
+    },
+    {
+      title: 'Click for Facebook',
+      url: 'https://google.com/',
+      start: '2024-12-28',
+      backgroundColor: 'red',
+    },
+  ];
+  return events;
+}
+
+function initializeCalendar() {
   const calendarEl = document.getElementById('calendar');
+  const data = getEvents();
   // eslint-disable-next-line no-undef
   const calendar = new FullCalendar.Calendar(calendarEl, {
     initialView: 'dayGridMonth',
@@ -12,54 +77,7 @@ function draw() {
       center: 'title',
       right: 'dayGridMonth,timeGridWeek,timeGridDay',
     },
-    events: [
-      {
-        title: 'All Day Event',
-        start: '2024-11-01',
-      },
-      {
-        title: 'Long Event',
-        start: '2024-11-07',
-        end: '2024-11-10',
-      },
-      {
-        groupId: '999',
-        title: 'Repeating Event',
-        start: '2024-11-09T16:00:00',
-      },
-      {
-        groupId: '999',
-        title: 'Repeating Event',
-        start: '2024-11-16T16:00:00',
-      },
-      {
-        title: 'Conference',
-        start: '2024-11-11',
-        end: '2024-11-13',
-      },
-      {
-        title: 'Meeting',
-        start: '2024-11-12T10:30:00',
-        end: '2024-11-12T12:30:00',
-      },
-      {
-        title: 'Lunch',
-        start: '2024-11-12T12:00:00',
-      },
-      {
-        title: 'Meeting',
-        start: '2024-11-12T14:30:00',
-      },
-      {
-        title: 'Birthday Party',
-        start: '2024-11-13T07:00:00',
-      },
-      {
-        title: 'Click for Google',
-        url: 'https://google.com/',
-        start: '2024-11-28',
-      },
-    ],
+    events: data,
   });
   calendar.render();
 }
@@ -68,7 +86,7 @@ export function loadfullcalendar() {
   const scriptfullcalendar = document.createElement('script');
   scriptfullcalendar.setAttribute('type', 'text/javascript');
   scriptfullcalendar.src = 'https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js';
-  scriptfullcalendar.addEventListener('load', draw);
+  scriptfullcalendar.addEventListener('load', initializeCalendar);
   document.head.append(scriptfullcalendar);
 }
 

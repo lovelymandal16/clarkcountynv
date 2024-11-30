a055fcc4d7d555ee197802fc7f448b75341b8a7a
Meet Kumar Jain <meejain@Meets-MacBook-Pro-2.local>
2024-11-30 19:06:05 +0530

    Initializing scripts for calendar

--- blocks/full-calendar/full-calendar.js
+++ blocks/full-calendar/full-calendar.js
@@ -1,19 +1,6 @@
 import { div } from '../../scripts/dom-helpers.js';
-import { loadScript } from '../../scripts/aem.js';
-
-async function fc1() {
-  await loadScript('https://cdn.jsdelivr.net/npm/fullcalendar/index.global.min.js');
-}
 
 export default async function decorate(block) {
   const calDiv = div({ id: 'calendar' });
   block.append(calDiv);
-  await fc1();
-  document.addEventListener('DOMContentLoaded', () => {
-    const calendarEl = document.getElementById('calendar');
-    const calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridMonth',
-    });
-    calendar.render();
-  });
 }

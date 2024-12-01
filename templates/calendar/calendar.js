d9aedff49f12d009038446ce224c81b107c394ee
Meet Kumar Jain <meejain@p01kbdb03.macromedia.com>
2024-12-02 03:26:43 +0530

    Instrumenting a Template - Calendar

--- templates/calendar/calendar.js
+++ templates/calendar/calendar.js
@@ -1,71 +1,80 @@
 import { div, section } from '../../scripts/dom-helpers.js';
+import { normalizeString } from '../../scripts/utils.js';
 
 // Scripts for Full Calendar
 
-function getEventsManual() {
-  const events = [
-    {
-      title: 'All Day Event',
-      start: '2024-12-01',
-      backgroundColor: '#3787d8',
-    },
-    {
-      title: 'Long Event',
-      start: '2024-12-07',
-      end: '2024-12-10',
-      backgroundColor: '#da80c1',
-    },
-    {
-      groupId: '999',
-      title: 'Repeating Event',
-      start: '2024-12-09T16:00:00',
-      backgroundColor: '#3a9500',
-      classNames: ['county_commissioners'],
-    },
-    {
-      groupId: '1000',
-      title: 'Repeating Event',
-      start: '2024-12-16T16:00:00',
-      end: '2024-12-16T18:00:00',
-      backgroundColor: '#3a9500',
-      borderColor: 'blue',
-    },
-    {
-      title: 'Conference',
-      start: '2024-12-11',
-      end: '2024-12-13',
-      backgroundColor: 'red',
-    },
-    {
-      title: 'Meeting',
-      start: '2024-12-12T10:30:00',
-      end: '2024-12-12T12:30:00',
-      backgroundColor: 'red',
-    },
-    {
-      title: 'Lunch',
-      start: '2024-12-12T12:00:00',
-      backgroundColor: 'red',
-    },
-    {
-      title: 'Meeting',
-      start: '2024-12-12T14:30:00',
-      backgroundColor: 'red',
-    },
-    {
-      title: 'Birthday Party',
-      start: '2024-12-13T07:00:00',
-      backgroundColor: 'red',
-    },
-    {
-      title: 'Click for Facebook',
-      url: 'https://google.com/',
-      start: '2024-12-28',
-      backgroundColor: 'red',
-    },
-  ];
-  return events;
-}
+// function getEventsManual() {
+//   const events = [
+//     {
+//       title: 'All Day Event',
+//       start: '2024-12-01',
+//       backgroundColor: '#3787d8',
+//     },
+//     {
+//       title: 'Long Event',
+//       start: '2024-12-07',
+//       end: '2024-12-10',
+//       backgroundColor: '#da80c1',
+//     },
+//     {
+//       groupId: '999',
+//       title: 'Repeating Event',
+//       start: '2024-12-09T16:00:00',
+//       backgroundColor: '#3a9500',
+//       classNames: ['county_commissioners'],
+//     },
+//     {
+//       groupId: '1000',
+//       title: 'Repeating Event',
+//       start: '2024-12-16T16:00:00',
+//       end: '2024-12-16T18:00:00',
+//       backgroundColor: '#3a9500',
+//       borderColor: 'blue',
+//     },
+//     {
+//       title: 'Conference',
+//       start: '2024-12-11',
+//       end: '2024-12-13',
+//       backgroundColor: 'red',
+//     },
+//     {
+//       title: 'Meeting',
+//       start: '2024-12-12T10:30:00',
+//       end: '2024-12-12T12:30:00',
+//       backgroundColor: 'red',
+//     },
+//     {
+//       title: 'Lunch',
+//       start: '2024-12-12T12:00:00',
+//       backgroundColor: 'red',
+//     },
+//     {
+//       title: 'Meeting',
+//       start: '2024-12-12T14:30:00',
+//       backgroundColor: 'red',
+//     },
+//     {
+//       title: 'Birthday Party',
+//       start: '2024-12-13T07:00:00',
+//       backgroundColor: 'red',
+//     },
+//     {
+//       title: 'Click for Facebook',
+//       url: 'https://google.com/',
+//       start: '2024-12-28',
+//       backgroundColor: 'red',
+//     },
+//   ];
+//   return events;
+// }
+
+const childCalendars = [
+  'County Commissioners',
+  'County Commission District A',
+  'County Commission District B',
+  'County Commissioners District C',
+  'County Commissioners District D',
+];
 
 // Fetching events from individual calendar sheets
 export async function fetchPlaceholders(prefix) {
@@ -98,21 +107,27 @@ export async function fetchPlaceholders(prefix) {
 }
 
 async function initializeCalendar() {
+  let eventsData = [];
   const calendarEl = document.getElementById('calendar');
   // const data = getEventsManual();
-  const placeholders = await fetchPlaceholders('county-commissioners');
-  const eventsData = placeholders.data;
-  // eslint-disable-next-line no-undef
-  const calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: 'dayGridMonth',
-    headerToolbar: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'dayGridMonth,timeGridWeek,timeGridDay',
-    },
-    events: eventsData,
+  childCalendars.forEach(async (childCalendar, i) => {
+    const normalizeCalendar = normalizeString(childCalendar);
+    const placeholders = await fetchPlaceholders(normalizeCalendar);
+    eventsData = [...eventsData, ...placeholders.data];
+    if (i === childCalendars.length - 1) {
+      // eslint-disable-next-line no-undef
+      const calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        headerToolbar: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,timeGridDay',
+        },
+        events: eventsData,
+      });
+      calendar.render();
+    }
   });
-  calendar.render();
 }
 
 export function loadfullcalendar() {
@@ -138,6 +153,4 @@ export default async function decorate(doc) {
   $calendarSection.append(calDiv);
   $main.append($calendarSection);
   loadfullcalendar();
-  const placeholders = await fetchPlaceholders('county-commissioners');
-  console.log(placeholders.data);
 }

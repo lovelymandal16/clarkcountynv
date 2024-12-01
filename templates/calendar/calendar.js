da0aace6b5277184c62fa39aa5ec340b8a3433b2
Meet Kumar Jain <meejain@p01kbdb03.macromedia.com>
2024-12-01 05:44:47 +0530

    Instrumenting a Template - Calendar

--- templates/calendar/calendar.js
+++ templates/calendar/calendar.js
@@ -2,7 +2,7 @@ import { div, section } from '../../scripts/dom-helpers.js';
 
 // Scripts for Full Calendar
 
-function getEvents() {
+function getEventsManual() {
   const events = [
     {
       title: 'All Day Event',
@@ -20,6 +20,7 @@ function getEvents() {
       title: 'Repeating Event',
       start: '2024-12-09T16:00:00',
       backgroundColor: '#3a9500',
+      classNames: ['county_commissioners'],
     },
     {
       groupId: '1000',
@@ -66,9 +67,41 @@ function getEvents() {
   return events;
 }
 
-function initializeCalendar() {
+// Fetching events from individual calendar sheets
+export async function fetchPlaceholders(prefix) {
+  window.placeholders = window.placeholders || {};
+  const TRANSLATION_KEY = prefix;
+  const loaded = window.placeholders[`${TRANSLATION_KEY}-loaded`];
+
+  if (!loaded) {
+    window.placeholders[`${TRANSLATION_KEY}-loaded`] = new Promise((resolve) => {
+      fetch(`/calendar/${prefix}.json?sheet=events`)
+        .then((resp) => {
+          if (resp.ok) {
+            return resp.json();
+          }
+          return {};
+        })
+        .then((json) => {
+          window.placeholders[prefix] = json;
+          resolve(window.placeholders[prefix]);
+        })
+        .catch(() => {
+          // error loading placeholders
+          window.placeholders[prefix] = {};
+          resolve(window.placeholders[prefix]);
+        });
+    });
+  }
+  await window.placeholders[`${TRANSLATION_KEY}-loaded`];
+  return window.placeholders[`${TRANSLATION_KEY}`];
+}
+
+async function initializeCalendar() {
   const calendarEl = document.getElementById('calendar');
-  const data = getEvents();
+  // const data = getEventsManual();
+  const placeholders = await fetchPlaceholders('county-commissioners');
+  const eventsData = placeholders.data;
   // eslint-disable-next-line no-undef
   const calendar = new FullCalendar.Calendar(calendarEl, {
     initialView: 'dayGridMonth',
@@ -77,7 +110,7 @@ function initializeCalendar() {
       center: 'title',
       right: 'dayGridMonth,timeGridWeek,timeGridDay',
     },
-    events: data,
+    events: eventsData,
   });
   calendar.render();
 }
@@ -105,4 +138,6 @@ export default async function decorate(doc) {
   $calendarSection.append(calDiv);
   $main.append($calendarSection);
   loadfullcalendar();
+  const placeholders = await fetchPlaceholders('county-commissioners');
+  console.log(placeholders.data);
 }
